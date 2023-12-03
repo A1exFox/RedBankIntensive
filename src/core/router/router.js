b57672da9377dad5d7359732372f9559d291bdbd
A1exFox <oxoma91@gmail.com>
2023-12-03 17:20:11 +0300

    feat: layout and links

--- src/core/router/router.js
+++ src/core/router/router.js
@@ -1,3 +1,4 @@
+import { Layout } from '@/components/layout/layout.component'
 import { NotFound } from '@/components/screens/not-found/not-found.component'
 
 import { ROUTES } from './routes.data'
@@ -5,12 +6,37 @@ import { ROUTES } from './routes.data'
 export class Router {
 	#routes = ROUTES
 	#currentRoute = null
+	#layout = null
+
 	constructor() {
+		window.addEventListener('popstate', () => {
+			this.#handleRouteChange()
+		})
 		this.#handleRouteChange()
+		this.#handleLinks()
 	}
+
+	#handleLinks() {
+		document.addEventListener('click', event => {
+			const target = event.target.closest('a')
+			if (target) {
+				event.preventDefault()
+				this.navigate(target.href)
+			}
+		})
+	}
+
+	navigate(path) {
+		if (path !== this.getCurrentPath) {
+			window.history.pushState({}, '', path)
+			this.#handleRouteChange()
+		}
+	}
+
 	getCurrentPath() {
 		return window.location.pathname
 	}
+
 	#handleRouteChange() {
 		const path = this.getCurrentPath() || '/'
 		let route = this.#routes.find(route => route.path === path)
@@ -21,10 +47,19 @@ export class Router {
 			}
 		}
 		this.#currentRoute = route
-		this.render()
+		this.#render()
 	}
-	render() {
+
+	#render() {
 		const component = new this.#currentRoute.component()
-		document.getElementById('app').innerHTML = component.render()
+		if (!this.#layout) {
+			this.#layout = new Layout({
+				router: this,
+				children: component.render()
+			})
+			document.getElementById('app').innerHTML = this.#layout.render()
+		} else {
+			document.querySelector('main').innerHTML = component.render()
+		}
 	}
 }
