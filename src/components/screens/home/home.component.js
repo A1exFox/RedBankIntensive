0d70b39260320279c36adf9fd5cf199cff52f167
A1exFox <oxoma91@gmail.com>
2023-12-05 17:23:08 +0300

    feat: create RQuery

--- src/components/screens/home/home.component.js
+++ src/components/screens/home/home.component.js
@@ -1,4 +1,5 @@
 import { BaseScreen } from '@/core/component/base-screen.component'
+import { $R } from '@/core/rquery/rquery.lib'
 import renderService from '@/core/services/render.service'
 
 import styles from './home.module.scss'
@@ -10,6 +11,7 @@ export class Home extends BaseScreen {
 	}
 	render() {
 		const element = renderService.htmlToElement(template, [], styles)
+		$R(element).find('h1').css('color', 'yellow')
 		return element.outerHTML
 	}
 }
